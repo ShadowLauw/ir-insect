2a6f71ba35c1f031f1134751ed1df8c8bcb44c1c
LauwRasp <laura.paraboschi@hotmail.com>
2025-08-11 14:13:56 +0200

    feat: Setup GUI buttons & labels
    - Image panel
    - Settings panel, including :
        - Palette control
        - Picture, Recording & CSV export
        - PWM control

--- gui.py
+++ gui.py
@@ -1,7 +1,99 @@
+from tkinter import *
+from tkinter import ttk, filedialog
+from PIL import Image, ImageTk
+from utils.colors import PALETTES
+import cv2
+
 
 class GUI:
     def __init__(self):
-        self.root = "ok"
+        self.root = Tk()
+        self.root.title("IR Insect Detector")
+        self.mainframe = ttk.Frame(self.root, padding="3 3 12 12")
+        self.mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
+        self.root.columnconfigure(0, weight=1)
+        self.root.rowconfigure(0, weight=1)
+
+        self.img_label = Label(self.mainframe)
+        self.img_label.grid(row=0, column=0)
+
+        self.palette, self.palette_name = PALETTES[0]
+
+        self.setup_settings_panel()
+
+    def setup_settings_panel(self):
+        self.settings_frame = ttk.Frame(self.mainframe)
+        self.settings_frame.grid(row=0, column=1, sticky=(N, S), padx=10)
+
+        self.setup_palette_selector()
+        self.setup_action_buttons()
+        self.setup_pwm_controls()
+
+    def setup_palette_selector(self):
+        ttk.Label(self.settings_frame, text="🎨 Select palette").pack(pady=5)
+        palette_names = [name for _, name in PALETTES]
+        self.palette_combo = ttk.Combobox(
+            self.settings_frame, values=palette_names, state="readonly"
+        )
+        self.palette_combo.set(self.palette_name)  # default value
+        self.palette_combo.pack(pady=5)
+        self.palette_combo.bind("<<ComboboxSelected>>", self.on_palette_selected)
+
+    def on_palette_selected(self):
+        index = self.palette_combo.current()
+        self.palette, self.palette_name = PALETTES[index]
+        # TODO update the image
+
+    def setup_action_buttons(self):
+        ttk.Button(
+            self.settings_frame, text="📸 Picture (P)", command=placeholder
+        ).pack(pady=5)
+        ttk.Button(self.settings_frame, text="🎥 Record (R)", command=placeholder).pack(
+            pady=5
+        )
+        ttk.Button(
+            self.settings_frame, text="📊 Export to CSV (E)", command=placeholder
+        ).pack(pady=5)
+
+    def setup_pwm_controls(self):
+        self.pwm_label = ttk.Label(self.settings_frame, text="PWM : Inactive (GPIO 18)")
+        self.pwm_label.pack(pady=5)
+        ttk.Button(self.settings_frame, text="Toggle PWM", command=placeholder).pack(
+            pady=5
+        )
+
+        # PWM Mode button
+        self.pwm_mode = StringVar(value="auto")
+        ttk.Checkbutton(
+            self.settings_frame,
+            text="PWM Auto mode",
+            command=placeholder,
+            variable=self.pwm_mode,
+            onvalue="auto",
+            offvalue="manual",
+        ).pack(pady=5)
+
+        # Frequency
+        ttk.Label(self.settings_frame, text="Frequency (Hz)").pack(pady=5)
+        self.pwm_freq_scale = ttk.Scale(
+            self.settings_frame, from_=500, to=1500, orient=HORIZONTAL
+        )
+        self.pwm_freq_scale.set(1000)
+        self.pwm_freq_scale.state(["disabled"])
+        self.pwm_freq_scale.pack(pady=5)
+
+        # Duty cycle
+        ttk.Label(self.settings_frame, text="Duty Cycle (%)").pack(pady=5)
+        self.pwm_duty_scale = ttk.Scale(
+            self.settings_frame, from_=0, to=100, orient=HORIZONTAL
+        )
+        self.pwm_duty_scale.set(50)
+        self.pwm_duty_scale.state(["disabled"])
+        self.pwm_duty_scale.pack(pady=5)
+
+    def close(self):
+        self.root.destroy()
 
     def run(self):
-        pass
+        self.root.protocol("WM_DELETE_WINDOW", self.close)
+        self.root.mainloop()
