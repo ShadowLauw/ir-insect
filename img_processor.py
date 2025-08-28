eaae9b43a764dd9a62852ac1f982fb542a94003f
RaspLauw <laura.paraboschi@hotmail.com>
2025-08-28 21:57:23 +0100

    feat: Circularity to detect insects
    Add circularity check on contours to reduce false positives.
    Detect insects only when blobs are sufficiently round, to match catadioptric
    reflections

--- img_processor.py
+++ img_processor.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 
 
 class ImageProcessor:
@@ -31,8 +32,21 @@ class ImageProcessor:
         contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
         for cnt in contours:
-            if cv2.contourArea(cnt) > 200:
+            area = cv2.contourArea(cnt)
+            if area < 200 or area > 2000: #To be adjusted
+                continue
+
+            # Perimeter computation
+            perimeter = cv2.arcLength(cnt, True)
+            if perimeter == 0:
+                continue
+            circularity = 4 * np.pi * (area / (perimeter * perimeter))
+
+            if circularity > 0.6:  # Catadioptric reflections tend to be circular
                 x, y, w, h = cv2.boundingRect(cnt)
                 cv2.rectangle(output_colormap, (x, y), (x + w, y + h), (0, 255, 0), 2)
+                cv2.putText(output_colormap, f"{circularity:.2f}",
+                            (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX, 
+                            0.5, (0, 255, 0), 1, cv2.LINE_AA)
 
         return output_colormap
